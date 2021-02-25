f5f48ecee5fefe50c25e03fb8cc9a16c6ec5f106
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-02-25 17:19:40 +0100

    Modificati colori con Javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 var nome = prompt("Come ti chiami?");
 var cognome = prompt("Qual è il tuo cognome?");
-var colore_preferito = prompt ("Inserisci il tuo colore preferito:");
+var colorePreferito = prompt ("Inserisci il tuo colore preferito:");
 
-document.getElementById("nome_utente").innerHTML= "Il tuo nome è: " +nome;
+// visualizza
 
-document.getElementById("cognome_utente").innerHTML= "Il tuo nome cognome è: " +cognome;
+document.getElementById("nomeUtente").innerHTML= "Il tuo nome è: " +nome;
+document.getElementById("cognomeUtente").innerHTML= "Il tuo nome cognome è: " +cognome;
+document.getElementById("coloreUtente").innerHTML= "Il tuo colore preferito è: " +colorePreferito;
+document.getElementById("numeroUtente").innerHTML= "il tuo codice accesso è: 27";
 
-document.getElementById("colore_utente").innerHTML= "Il tuo colore preferito è: " +colore_preferito;
+// color
 
-document.getElementById("numero_utente").innerHTML= "il tuo codice accesso è: 27" ;
+document.getElementById("nomeUtente").style.color = "yellowgreen";
+document.getElementById("cognomeUtente").style.color = "deepskyblue";
+document.getElementById("coloreUtente").style.color = "plum";
+document.getElementById("numeroUtente").style.color = "lightcoral";
