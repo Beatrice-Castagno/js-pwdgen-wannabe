cd5544ca87169c6a536217add45750a2b52b72bb
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-02-25 16:54:13 +0100

    Primo esercizio Javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var nome = prompt("Come ti chiami?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colore_preferito = prompt ("Inserisci il tuo colore preferito:");
+
+document.getElementById("nome_utente").innerHTML= "Il tuo nome è: " +nome;
+
+document.getElementById("cognome_utente").innerHTML= "Il tuo nome cognome è: " +cognome;
+
+document.getElementById("colore_utente").innerHTML= "Il tuo colore preferito è: " +colore_preferito;
+
+document.getElementById("numero_utente").innerHTML= "il tuo codice accesso è: 27" ;
